24f8f5fe39c96a231e183580a2008c378877c7a5
Aditee Katti <aditeekatti@gmail.com>
2023-04-07 10:55:16 -0700

    basic setup

---
+++ my-to-do-app/src/ToDoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TodoList() {
+  return (
+    <div>
+      <h1>My To-Do List</h1>
+      <ul>
+        <li>Wake Up</li>
+        <li>Toilet</li>
+        <li>Brush</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
